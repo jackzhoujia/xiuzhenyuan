665e343c970620d32631836d2b5588e1bb05f370
zhoujiajia <1036726933@qq.com>
2019-04-04 22:44:58 +0800

    a
    Signed-off-by: zhoujiajia <1036726933@qq.com>

---
+++ TaskFourTeen/TaskFourTeen.js
@@ -0,0 +1,8 @@
+function showDrop() {
+    var dd = document.getElementsByClassName("drop-down");
+    if (dd[0].style.display == "none") {
+        dd[0].style.display = "block";
+    } else {
+        dd[0].style.display = "none";
+    }
+}
